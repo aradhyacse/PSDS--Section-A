90d1cf970619b56fbac28eced2f61392aa761b83
Anant Chauhan <chauhananant32@gmail.com>
2026-08-04 16:46:27 +0530

    Add files via upload

--- linear and Binary.cpp
+++ linear and Binary.cpp
@@ -1,3 +1,5 @@
+// LINEAR SEARCH--------------------------------------------
+
 #include <iostream>
 using namespace std;
 int main() {
@@ -15,6 +17,40 @@ int main() {
         if (arr[i] == key) {
             cout << "Element found at index: " << i;
             return 0;
+        }
+    }
+    cout << "Element not found (-1)";
+    return 0;
+}
+
+
+
+// BINARY SEARCH---------------------------------------------------
+#include <iostream>
+using namespace std;
+int main() {
+    int n, key;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter sorted array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Enter element to find: ";
+    cin >> key;
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            cout << "Element found at index: " << mid;
+            return 0;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
         }
     }
     cout << "Element not found (-1)";
